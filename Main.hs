0c1746ddaa4bc421dc55ad4542e95860fda96fad
Mike Ledger <mike@quasimal.com>
2018-04-06 18:41:47 +1000

    simplify deciding whether to convert/copy/ignore

--- Main.hs
+++ Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE NamedFieldPuns    #-}
 {-# LANGUAGE OverloadedStrings #-}
 import           Control.Concurrent.ParallelIO.Local
@@ -20,9 +21,14 @@ data Opts = Opts
   , jobs    :: !Int
   }
 
+data Conversion
+  = Ignored !FilePath
+  | Copied !FilePath !FilePath
+  | Converted !FilePath !FilePath
+  deriving (Show)
+
 data ConvertProgress = ConvertProgress
-  { message      :: !Text
-  , start        :: {-# UNPACK #-} !UTCTime
+  { start        :: {-# UNPACK #-} !UTCTime
   , now          :: {-# UNPACK #-} !UTCTime
   , convprogress :: {-# UNPACK #-} !Progress
   }
@@ -33,234 +39,77 @@ instance HasProgress ConvertProgress where
 f2t :: FilePath -> Text
 f2t = either id id . toText
 
-inc :: ProgressRef ConvertProgress -> () -> IO ()
-inc ref message = do
+inc :: ProgressRef ConvertProgress -> Action -> (FilePath, FilePath) -> IO ()
+inc ref _dec _frto = do
   now <- date
   incProgress ref $ \c -> c
-    { message = "ok"
-    , now
+    { now
     , convprogress = (convprogress c)
-      { progressDone = progressDone (convprogress c) + 1
-      }
+      { progressDone = progressDone (convprogress c) + 1 }
     }
+  {-
+  where
+    mkConversion Copy (from, to)    = Copied from to
+    mkConversion Ignore (from, _)   = Ignored from
+    mkConversion Convert (from, to) = Converted from to -}
 
-shouldSkip :: FilePath -> FilePath -> IO Bool
-shouldSkip from to =
-  if case extension from of
-    Just "m3u"  -> True
-    Just "pls"  -> True
-    Just "asx"  -> True
-    Just "xspf" -> True
-    Just "wpl"  -> True
-    _           -> False
-  then return True
-  else testfile to
+data Action
+  = Copy
+  | Convert
+  | Ignore
 
-shouldCopy :: FilePath -> FilePath -> IO Bool
-shouldCopy from _ = case extension from of
-  Just ext -> return (Set.notMember (T.toUpper ext) musicExtensions)
-  _        -> return True
+decide :: FilePath -> IO Action
+decide from
+  = ignoreDot (encodeString from)
+  $ ignoreDot (encodeString (filename from))
+  $ ignorePlaylists
+  $ copyMusic
+  $ convertMusic
+  $ copyImages
+  $ return Ignore
+  where
+    ignoreDot ('.':_) = const (return Ignore)
+    ignoreDot _       = id
+    testExt exts r = case extension from of
+      Just ext | Set.member (T.toLower ext) exts -> const (return r)
+      _        -> id
+    ignorePlaylists = testExt playlistExts Ignore
+    convertMusic = testExt musicExts Convert
+    copyMusic = testExt keepMusicExts Copy
+    copyImages = testExt imageExts Copy
 
-canCopy :: FilePath -> FilePath -> IO Bool
-canCopy from _ = case extension from of
-  Just ext -> return (Set.member (T.toLower ext) imageExtensions)
-  _        -> return False
+convertOrCopy :: Opts -> (FilePath, FilePath) -> IO Action
+convertOrCopy Opts{bitrate} (from, to) = decide from >>= \case
+  Ignore  -> return Ignore
+  Copy    -> Copy <$ procs "cp" [f2t from, f2t to] mempty
+  Convert -> do
+    code <- proc
+      "ffmpeg"
+      [ "-loglevel", "quiet"
+      , "-i" , f2t from
+      , "-codec:a", "libopus"
+      , "-b:a", bitrate
+      , "-vbr", "on"
+      , "-compression_level", "10"
+      , "-map_metadata", "0:g"
+      , "-n"
+      , f2t (replaceExtension to "ogg")
+      ]
+      mempty
+    case code of
+      ExitSuccess   -> return Convert
+      ExitFailure _ -> Copy <$ procs "cp" [f2t from, f2t to] mempty
 
-musicExtensions :: Set.Set Text
-musicExtensions = Set.fromList
-  [ "AMR"
-  , "MP1"
-  , "MP2"
-  , "MP3"
-  , "SPX"
-  , "GSM"
-  , "WMA"
-  , "AAC"
-  , "MPC"
-  , "VQF"
-  , "OTS"
-  , "VOX"
-  , "VOC"
-  , "DWD"
-  , "SMP"
-  , "OGG"
-  , "RA"
-  , "RM"
-  , "FLAC"
-  , "LA"
-  , "PAC"
-  , "APE"
-  , "OFR"
-  , "OFS"
-  , "OFF"
-  , "RKA"
-  , "SHN"
-  , "TAK"
-  , "TTA"
-  , "WV"
-  , "WMA"
-  , "BRSTM"
-  , "DTS"
-  , "DTSHD"
-  , "DTSMA"
-  , "AST"
-  , "AW"
-  , "PSF"
-  ]
-
-imageExtensions :: Set.Set Text
-imageExtensions = Set.fromList
-  [ "ase"
-  , "art"
-  , "bmp"
-  , "blp"
-  , "cd5"
-  , "cit"
-  , "cpt"
-  , "cr2"
-  , "cut"
-  , "dds"
-  , "dib"
-  , "djvu"
-  , "egt"
-  , "exif"
-  , "gif"
-  , "gpl"
-  , "grf"
-  , "icns"
-  , "ico"
-  , "iff"
-  , "jng"
-  , "jpeg"
-  , "jpg"
-  , "jfif"
-  , "jp2"
-  , "jps"
-  , "lbm"
-  , "max"
-  , "miff"
-  , "mng"
-  , "msp"
-  , "nitf"
-  , "ota"
-  , "pbm"
-  , "pc1"
-  , "pc2"
-  , "pc3"
-  , "pcf"
-  , "pcx"
-  , "pdn"
-  , "pgm"
-  , "PI1"
-  , "PI2"
-  , "PI3"
-  , "pict"
-  , "pct"
-  , "pnm"
-  , "pns"
-  , "ppm"
-  , "psb"
-  , "psd"
-  , "pdd"
-  , "psp"
-  , "px"
-  , "pxm"
-  , "pxr"
-  , "qfx"
-  , "raw"
-  , "rle"
-  , "sct"
-  , "sgi"
-  , "rgb"
-  , "int"
-  , "bw"
-  , "tga"
-  , "tiff"
-  , "tif"
-  , "vtf"
-  , "xbm"
-  , "xcf"
-  , "xpm"
-  , "3dv"
-  , "amf"
-  , "ai"
-  , "awg"
-  , "cgm"
-  , "cdr"
-  , "cmx"
-  , "dxf"
-  , "e2d"
-  , "egt"
-  , "eps"
-  , "fs"
-  , "gbr"
-  , "odg"
-  , "svg"
-  , "stl"
-  , "vrml"
-  , "x3d"
-  , "sxd"
-  , "v2d"
-  , "vnd"
-  , "wmf"
-  , "emf"
-  , "art"
-  , "xar"
-  , "png"
-  , "webp"
-  , "jxr"
-  , "hdp"
-  , "wdp"
-  , "cur"
-  , "ecw"
-  , "iff"
-  , "lbm"
-  , "liff"
-  , "nrrd"
-  , "pam"
-  , "pcx"
-  , "pgf"
-  , "sgi"
-  , "rgb"
-  , "rgba"
-  , "bw"
-  , "int"
-  , "inta"
-  , "sid"
-  , "ras"
-  , "sun"
-  , "tga"
-  ]
-
-convertOrCopy :: Opts -> (FilePath, FilePath) -> IO ()
-convertOrCopy Opts{bitrate} (from, to) = do
-  skip_ <- shouldSkip from to
-  copy_ <- shouldCopy from to
-  unless skip_ $ do
-    copyOnly <-
-      if copy_
-      then return True
-      else do
-        code <- proc
-          "ffmpeg"
-          [ "-loglevel", "quiet"
-          , "-i" , f2t from
-          , "-codec:a", "libopus"
-          , "-b:a", bitrate
-          , "-vbr", "on"
-          , "-compression_level", "10"
-          , "-map_metadata", "0:g"
-          , "-n"
-          , f2t (replaceExtension to "ogg")
-          ]
-          mempty
-        case code of
-          ExitSuccess   -> return False
-          ExitFailure _ -> return True
-    can <- canCopy from to
-    when
-      (copyOnly && can)
-      (procs "cp" [f2t from, f2t to] mempty)
+mapBadChars :: FilePath -> FilePath
+mapBadChars orig =
+  if null onlyBads
+  then orig
+  else decodeString (concatMap unbad str)
+  where
+    bads = "?<>\\" :: String
+    unbad c = if c `elem` bads then [] else [c]
+    str = encodeString orig
+    onlyBads = filter (`elem` bads) str
 
 main :: IO ()
 main = do
@@ -276,7 +125,7 @@ main = do
 
   let filesAndRels = [ (absf, fromJust (stripPrefix input absf)) | absf <- absFiles ]
   let dirs = Set.fromList (map ((output </>) . directory . snd) filesAndRels)
-  let files = map (\(absf, relf) -> (absf, output </> relf)) filesAndRels
+  let files = map (\(absf, relf) -> (absf, output </> mapBadChars relf)) filesAndRels
   mapM_ mktree dirs
   start0 <- date
 
@@ -291,19 +140,48 @@ main = do
           elapsed = realToFrac (diffUTCTime now start)
           avg = elapsed / fromInteger progressDone
           remain = avg * fromInteger (progressTodo - progressDone)
-
         in P.printf "%.2fs elapsed. %0.2fs remaining" elapsed remain)
     40
     ConvertProgress
-    { message = ""
-    , start = start0
+    { start = start0
     , now = start0
     , convprogress = Progress
       { progressDone = 0
       , progressTodo = toInteger (length files)
       }
     }
-
   withPool jobs $ \pool ->
-    parallel_ pool (map (convertOrCopy opts >=> inc progRef) files)
+    parallel_ pool (map (\q -> do dec <- convertOrCopy opts q
+                                  inc progRef dec q) files)
+
+playlistExts :: Set.Set Text
+playlistExts = Set.fromList
+  ["m3u", "pls", "asx", "xspf", "wpl"]
+
+musicExts :: Set.Set Text
+musicExts = Set.fromList
+  [ "amr", "mp1", "mp2", "mp3", "spx", "gsm", "wma", "aac", "mpc", "vqf"
+  , "ots", "vox", "voc", "dwd", "smp", "ogg", "ra", "rm", "flac", "la"
+  , "pac", "ape", "ofr", "ofs", "off", "rka", "shn", "tak", "tta", "wv"
+  , "wma", "brstm", "dts", "dtshd", "dtsma", "ast", "aw", "psf"]
+
+keepMusicExts :: Set.Set Text
+keepMusicExts = Set.fromList
+  ["ogg", "mp3", "opus", "aac"]
+
+imageExts :: Set.Set Text
+imageExts = Set.fromList
+  [ "ase", "art", "bmp", "blp", "cd5", "cit", "cpt", "cr2", "cut"
+  , "dds", "dib", "djvu", "egt", "exif", "gif", "gpl", "grf", "icns"
+  , "ico", "iff", "jng", "jpeg", "jpg", "jfif", "jp2", "jps", "lbm"
+  , "max", "miff", "mng", "msp", "nitf", "ota", "pbm", "pc1", "pc2"
+  , "pc3", "pcf", "pcx", "pdn", "pgm", "PI1", "PI2", "PI3", "pict", "pct"
+  , "pnm", "pns", "ppm", "psb", "psd", "pdd", "psp", "px", "pxm", "pxr"
+  , "qfx", "raw", "rle", "sct", "sgi", "rgb", "int", "bw", "tga"
+  , "tiff", "tif", "vtf", "xbm", "xcf", "xpm", "3dv", "amf", "ai", "awg"
+  , "cgm", "cdr", "cmx", "dxf", "e2d", "egt", "eps", "fs", "gbr", "odg"
+  , "svg", "stl", "vrml", "x3d", "sxd", "v2d", "vnd", "wmf", "emf"
+  , "art", "xar", "png", "webp", "jxr", "hdp", "wdp", "cur", "ecw", "iff"
+  , "lbm", "liff", "nrrd", "pam", "pcx", "pgf", "sgi", "rgb", "rgba"
+  , "bw", "int", "inta", "sid", "ras", "sun", "tga" ]
 

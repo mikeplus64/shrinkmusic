cbe1d5723938e9b7959a60302b06dec295f83dcc
Mike Ledger <eleventynine@gmail.com>
2018-09-12 01:10:48 +1000

    clean-up

--- default.nix
+++ default.nix
@@ -1,11 +1,10 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
 let
   inherit (nixpkgs) pkgs;
-  f = import ./shrinkmusic.nix;
   haskellPackages =
     if compiler == "default"
     then pkgs.haskellPackages
     else pkgs.haskell.packages.${compiler};
-  drv = haskellPackages.callPackage f {};
+  drv = haskellPackages.callCabal2nix "shrinkmusic" ./. {};
 in
   drv

0a65c4f38d4f28f1b4e633ed72836ccc5aad4b24
Mike Ledger <eleventynine@gmail.com>
2018-09-12 12:53:55 +1000

    pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,4 +1,13 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+{
+  config ? {},
+  nixpkgs ? import ((import <nixpkgs> config).fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs-channel";
+    rev = "6f3bd5db2f4d1f8bf21631ebe56f496114637c56";
+    sha256 = "1izdsqiyxb1bn9a4xl06hh14xhcc2rhcqcqfxwpf4q5i1q32bw5d";
+  }) config,
+  compiler ? "default"
+}:
 let
   inherit (nixpkgs) pkgs;
   haskellPackages =
